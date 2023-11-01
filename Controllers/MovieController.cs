939b3c59f7733c39eef12168746926e0f1ab4558
Melory Ayala <melory.ayala@gmail.com>
2023-11-01 16:01:45 +0100

    Creating  POST add movie

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using movies_api.Models;
+
 namespace movies_api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class MovieController : ControllerBase
 {
-    
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+            Console.WriteLine(movie.Duration);
+        }
+    }
 }
