16bae45d33f4bac2bcc7aa2ba04ad167e69f600e
Melory Ayala <melory.ayala@gmail.com>
2023-11-01 16:04:13 +0100

    Creating Movie and validation with Data Annotation

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+namespace movies_api.Models;
+
+public class Movie
+{
+    [Required(ErrorMessage = "The movie title is required")]
+    public string Title { get; set; }
+    
+    [Required(ErrorMessage = "The movie genre is required")]
+    [MaxLength(50, ErrorMessage = "The genre can not have more than 50 characters")]
+    public string Genre { get; set; }
+
+    [Required(ErrorMessage = "The movie duration is required")]
+    [Range(70, 600, ErrorMessage = "The movie duration must have between 70 and 600 minutes")]
+    public int Duration { get; set; }
+}
