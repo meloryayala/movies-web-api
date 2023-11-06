70c45691926ce2aebc3112e9a7f169653719bbf7
Melory Ayala <melory.ayala@gmail.com>
2023-11-06 18:14:06 +0100

    Adding entity framework proxis and setup useLazyProxies

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("MovieConnection");
 
 builder.Services.AddDbContext<MovieContext>(opts =>
-    opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    opts.UseLazyLoadingProxies().UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
