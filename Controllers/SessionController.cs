08a9f8077d301fd287d586dd508476767fe2547c
Melory Ayala <melory.ayala@gmail.com>
2023-11-06 20:07:58 +0100

    Creating session controller POST, GET and GET by id

---
+++ Controllers/SessionController.cs
@@ -0,0 +1,45 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using movies_api.Data;
+using movies_api.Data.Dtos;
+using movies_api.Models;
+
+namespace movies_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SessionController : ControllerBase
+{
+    private MovieContext _context;
+    private IMapper _mapper;
+
+    public SessionController(MovieContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult AddSession([FromBody] CreateSessionDto sessionDto)
+    {
+        Session session = _mapper.Map<Session>(sessionDto);
+        _context.Sessions.Add(session);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(ReadSessionById), new { Id = session.Id }, session);
+    }
+
+    [HttpGet]
+    public IEnumerable<ReadSessionDto> ReadSessions([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return _mapper.Map<List<ReadSessionDto>>(_context.Adresses.Skip(skip).Take(take));
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ReadSessionById(int id)
+    {
+        var session = _context.Sessions.FirstOrDefault(session => session.Id == id);
+        if (session == null) return NotFound();
+        ReadSessionDto sessionDto =  _mapper.Map<ReadSessionDto>(session);
+        return Ok(sessionDto);
+    }
+}
