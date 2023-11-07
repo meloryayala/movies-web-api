a0fc1a54a7206958c7ec11d7993c41be37439bb1
Melory Ayala <melory.ayala@gmail.com>
2023-11-07 15:47:41 +0100

    Migrating fix nullable cineId

---
+++ Migrations/20231107144528_NullCineId.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace movies_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullCineId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sessions_Cines_CineId",
+                table: "Sessions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CineId",
+                table: "Sessions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sessions_Cines_CineId",
+                table: "Sessions",
+                column: "CineId",
+                principalTable: "Cines",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sessions_Cines_CineId",
+                table: "Sessions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CineId",
+                table: "Sessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sessions_Cines_CineId",
+                table: "Sessions",
+                column: "CineId",
+                principalTable: "Cines",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
