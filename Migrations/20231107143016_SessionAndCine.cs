800f29e0be35f2fe82d54e0b05db999a9ef8c6d1
Melory Ayala <melory.ayala@gmail.com>
2023-11-07 15:36:59 +0100

    Migrating and updating the database

---
+++ Migrations/20231107143016_SessionAndCine.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace movies_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SessionAndCine : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CineId",
+                table: "Sessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_CineId",
+                table: "Sessions",
+                column: "CineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sessions_Cines_CineId",
+                table: "Sessions",
+                column: "CineId",
+                principalTable: "Cines",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sessions_Cines_CineId",
+                table: "Sessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sessions_CineId",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "CineId",
+                table: "Sessions");
+        }
+    }
+}
